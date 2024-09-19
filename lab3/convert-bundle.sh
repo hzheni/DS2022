e978c6124cf3d442259c12f61725117e4fd5fadf
Jessica Ni <jessicani@Jessicas-MBP.hsd1.va.comcast.net>
2024-09-19 17:32:50 -0400

    part 2 of lab

--- lab3/convert-bundle.sh
+++ lab3/convert-bundle.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-curl -o https://s3.amazonaws.com/ds2002-resources/labs/lab3-bundle.tar.gz
+wget https://s3.amazonaws.com/ds2002-resources/labs/lab3-bundle.tar.gz
 tar -xzvf lab3-bundle.tar.gz
 
 # awk can remove spaces
-awk '!/^[[:space:]]*$/' myfile.tsv
+awk '!/^[[:space:]]*$/' lab3_data.tsv
+
+echo my_new_file.tsv
 
 # Use `tr` - "translate"
-tr '\t' ',' < file.tsv > file.csv
+tr '\t' ',' < lab3_data.tsv > file.csv
 
-lines=$(tail -n +2 myfile.csv | wc -l)
+
+
+lines=$(tail -n +2 file.csv | wc -l)
 echo $lines
 
-tar -czvf converted-archive.tar.gz myfile.csv
+tar -czvf converted-archive.tar.gz file.csv
+
+
+#tsv_file = 'lab3_data.tsc'
+#df = pd.read_csv(tsv_file, sep='\t')
+#csv_file = 'myfile.csv'
+#df.to_csv(csv_file, index=False)
