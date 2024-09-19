0413270a9d90e0a99e204f2f51379791d76ff60e
Jessica Ni <jessicani@Jessicas-MBP.hsd1.va.comcast.net>
2024-09-19 12:49:28 -0400

    working version of classmate list

---
+++ my-work/chatlist-classmates.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# move into the people dir from my-work
+cd ../people
+
+# look for this file
+target_file="README.md"
+
+# loop through the dirs and find the target file
+for dir in $( find . -type d); do
+  if [ -f "$dir/$target_file" ]; then
+     # echo "$dir/$target_file"
+     name=$(head -n 1 "$dir/$target_file")
+     echo "$name"
+  fi
+done
